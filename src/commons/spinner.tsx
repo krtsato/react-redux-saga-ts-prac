e4c3cf38e586022beb52c4f6f9348dc1861b0551
krtsato <ev60596@meiji.ac.jp>
2020-02-08 12:59:41 +0900

    feat: add spinner

--- src/commons/spinner.tsx
+++ src/commons/spinner.tsx
@@ -1,15 +1,4 @@
 import React, {FC} from "react"
+import CircularProgress from "@material-ui/core/CircularProgress"
 
-export const Spinner: FC = () => <h1>Loading Now...</h1>
-
-/*
-import { Dimmer, Loader, Segment } from 'semantic-ui-react';
-
-const Spinner: FC = () => (
-  <Segment className="spinner">
-    <Dimmer active inverted>
-      <Loader inverted={false}>読み込み中...</Loader>
-    </Dimmer>
-  </Segment>
-);
-*/
+export const Spinner: FC = () => <CircularProgress />
